b97f47c123b6860a664624293503305a1037ea78
Asakundwi Mavhungu <mavhasa17@gmail.com>
2024-06-17 23:02:19 +0200

    added route for atest view

--- moonstone-tech-assesment/routes/web.php
+++ moonstone-tech-assesment/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AtestController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/atest', [AtestController::class, 'index']);
