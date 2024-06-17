f8a2f9c71285f5dfaa5250cad43496d71657ca9b
Asakundwi Mavhungu <mavhasa17@gmail.com>
2024-06-17 23:51:35 +0200

    fixed error with comments

--- moonstone-tech-assesment/resources/views/atest/dropdown-item.blade.php
+++ moonstone-tech-assesment/resources/views/atest/dropdown-item.blade.php
@@ -1,9 +1,9 @@
 <li>
     <span class="caret">{{ $item->name }}</span>
-    // if item->children is a set, include the nested list
+    <!-- if item->children is a set, include the nested list --> 
     @if (isset($item->children))
         <ul class="nested">
-            // for each child in the list, recursively add that child-item's children
+            <!-- for each child in the list, recursively add that child-item's children --> 
             @foreach ($item->children as $child)
                 @include('atest.dropdown-item', ['item' => $child])
             @endforeach
