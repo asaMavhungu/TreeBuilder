d5ff348a876c268f5bc924e67293ca1d2486a4d8
Asakundwi Mavhungu <mavhasa17@gmail.com>
2024-06-18 04:05:17 +0200

    prevent a node from being added multiple times

--- moonstone-tech-assesment/app/Http/Controllers/AtestController.php
+++ moonstone-tech-assesment/app/Http/Controllers/AtestController.php
@@ -39,7 +39,8 @@ class AtestController extends Controller
         {
             $currentItem = $item;
 
-            $tree->push($currentItem);
+            if (!$visitetedItems->contains($item->id))
+                $tree->push($currentItem);
 
 
             while ($currentItem)
