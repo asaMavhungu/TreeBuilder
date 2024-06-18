3be031fe4d5683fea44e0d5715eabf74dd484761
Asakundwi Mavhungu <mavhasa17@gmail.com>
2024-06-18 03:56:41 +0200

    added filter system

--- moonstone-tech-assesment/app/Http/Controllers/AtestController.php
+++ moonstone-tech-assesment/app/Http/Controllers/AtestController.php
@@ -11,11 +11,60 @@ class AtestController extends Controller
     public function index()
     {
         $items = Atest::all();
+
+        $query = "clear";
+
+        //$tree = $this->buildTree($items);
+        $tree = $this->filterTree( $items, $query );
+        $tree = $this->buildTree($tree);
         $tree = $this->buildTree($items);
 
         // pass the tree structure to the 'atest.index' view
         // the tree structure will be refered to as 'tree' in the view
         return view('atest.index', compact('tree')); 
+    }
+
+    private function filterTree($items, $query)
+    {
+
+        // filter out all the items that have the query as a substring
+        $filteredItems = $items->filter(function ($item) use ($query) {
+            return stripos($item->name, $query) !== false;
+        });
+
+        $tree = collect();
+        // keep track of nodes already added to the tree
+        $visitetedItems = collect();
+
+        foreach ($filteredItems as $item) 
+        {
+            $currentItem = $item;
+
+
+            while ($currentItem)
+            {
+
+                // get this items parent
+                $parent = Atest::where('id', $currentItem->pef_item_id)->first();
+
+                if ($parent)
+                {
+                    if (!$visitetedItems->contains($parent->id))
+                    {
+                        $tree->push($parent); // push this parent to the tree
+                        $visitetedItems->push($parent->id); // mark parent as visited
+                    }
+                    $currentItem = $parent; // now we move to the parents of current parent
+                }
+                else
+                {
+                    $tree->push($currentItem); // no parent means this is a root node
+                    break; // move to the next filteredIten
+                }
+            }
+
+        }
+        return $tree;
     }
 
     private function buildTree($items)
